aeddd808557e5ff902010fd71770887bcd59178a
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-08-11 23:03:57 +0700

    add min routes, morgan, nodemon

---
+++ api/routes/orders.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders were fetched'
+  })
+});
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Orders was created'
+  })
+});
+  
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders deleted',
+    orderId: req.params.orderId
+  })
+});
+
+
+module.exports = router;
