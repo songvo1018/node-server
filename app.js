42708c33c72e8846d2a8264ee396721fd2f55608
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-08-22 12:10:42 +0700

    patch method from mongoose added to api

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+
+mongoose.connect('mongodb+srv://songvo:' + process.env.MONGODB_ATLAS_PW +'@cluster0.9bzjf.azure.mongodb.net/server-with-mongo?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
