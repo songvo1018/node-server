a9912e5b932126858da6dec2231f7c7ada67d1a4
Leha <song.vo@yandex.ru>
2020-08-28 23:19:54 +0700

    Implementing upload file functionality

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toLocaleDateString() + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' | file.mimetype === 'image/png') {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+
+}
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 1024 * 1024 * 6
+  },
+  fileFilter: fileFilter
+});
 
 const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
   Product.find()
-    .select('name price _id')
+    .select('name price _id productImage')
     .exec()
     .then(docs => {
       const response = {
@@ -15,6 +42,7 @@ router.get('/', (req, res, next) => {
           return {
             name: doc.name,
             price: doc.price,
+            productImage: doc.productImage,
             _id: doc._id,
             request: {
               type: 'GET', 
@@ -37,62 +65,65 @@ router.get('/', (req, res, next) => {
     })
 });
 
-router.post('/', (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price
-  })
-  product
-    .save()
-    .then( result => {
-    console.log(result);
-    res.status(201).json({
-      message: 'Created product successfully',
-      createdProduct: {
-        name: result.name,
-        price: result.price,
-        _id: result._id,
-        request: {
-          type: 'GET',
-          url: 'http://localhost:3000/products/' + result._id
-        }
-      }
-    })
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      })
-    });
-  
+router.post("/", upload.single('productImage'), (req, res, next) => {
+  console.log(req.file);
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
+		name: req.body.name,
+    price: req.body.price,
+    productImage: req.file.path
+	});
+	product
+		.save()
+		.then((result) => {
+			console.log(result);
+			res.status(201).json({
+				message: "Created product successfully",
+				createdProduct: {
+					name: result.name,
+					price: result.price,
+					_id: result._id,
+					request: {
+						type: "GET",
+						url: "http://localhost:3000/products/" + result._id,
+					},
+				},
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				error: err,
+			});
+		});
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
-    .select('name price _id')
-    .exec()
-    .then(doc => {
-      console.log("From database", doc);
-      if (doc) {
-        res.status(200).json({
-          product: doc,
-          request: {
-            type: 'GET',
-            description: 'GET_ALL_PRODUCTS',
-            url: 'http://localhost:3000/products/'
-          }
-        })
-      } else {
-        res.status(404).json({message: "No valid entry found for provided ID"})
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({error: err})
-    })
+		.select("name price _id productImage")
+		.exec()
+		.then((doc) => {
+			console.log("From database", doc);
+			if (doc) {
+				res.status(200).json({
+					product: doc,
+					request: {
+						type: "GET",
+						description: "GET_ALL_PRODUCTS",
+						url: "http://localhost:3000/products/",
+					},
+				});
+			} else {
+				res
+					.status(404)
+					.json({ message: "No valid entry found for provided ID" });
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({ error: err });
+		});
 })
 
 router.patch('/:productId', (req, res, next) => {
