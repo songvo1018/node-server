82280f40c41c76048dc16a4ac1ef7bd4d0e3a100
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-11-09 12:02:04 +0700

    1

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json({limit: '2mb'}))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (res.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, PUT, DELETE')
     return res.status(200).json({})
   }
   next();
