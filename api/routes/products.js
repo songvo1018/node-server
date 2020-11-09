ea0a522aaa4fa2be8c44de864a55de2b1c81d051
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-11-09 11:20:06 +0700

    1

--- api/routes/products.js
+++ api/routes/products.js
@@ -151,8 +151,9 @@ router.patch('/:productId', (req, res, next) => {
 })
 
 router.delete('/:productId', (req, res, next) => {
+  console.log('delete', req.params);
   const id = req.params.productId;
-  Product.remove({_id: id})
+  Product.deleteOne({_id: id})
     .exec()
     .then(result => {
       res.status(200).json({
