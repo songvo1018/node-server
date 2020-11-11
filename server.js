6e6de3cb5bff8c7b4205eab884269b51f791f604
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-11-11 13:11:31 +0700

    add https

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
 const port = 5000;
 
-const server = http.createServer(app)
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+
+const server = https.createServer(options, app)
 
 server.listen(port)
